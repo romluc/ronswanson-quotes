66f8fff7d36856fd9ef170f140fe8ba1ab5fa032
romluc <romulofilhofilho@gmail.com>
2020-05-21 15:51:17 -0300

    Create Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import './App.css';
+
+import Header from './components/Header';
 
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <div className='container'>
+      <Header className='title' />
+    </div>
+  );
 };
 
 export default App;
